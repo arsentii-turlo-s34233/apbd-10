d850396c6769b23a5f305b28d27b35040388656a
Arsentii Turlo <s34233@pjwstk.edu.pl>
2026-06-02 13:40:04 +0200

    Fixed redirecting in Program.Cs

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -112,6 +112,7 @@ public class AccountController : Controller
             CookieAuthenticationDefaults.AuthenticationScheme,
             principal
         );
+        
 
         return RedirectToAction("Index", "Dashboard");
     }
